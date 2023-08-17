11ddc42914db9c29fe906cadef930bad730c03f2
Natasha Ferguson <nata.ferguson@outlook.com>
2023-08-16 21:21:16 -0500

    Added validation on expense date.

--- app/src/main/java/com/example/businessexpensetracker/UI/ExpenseDetails.java
+++ app/src/main/java/com/example/businessexpensetracker/UI/ExpenseDetails.java
@@ -75,25 +75,67 @@ public class ExpenseDetails extends AppCompatActivity {
                 // Fetch the expense date
                 String expenseDate = editDate.getText().toString();
 
-                if (id == -1) {
-                    expense = new Expense(0, editName.getText().toString(), Double.parseDouble(editPrice.getText().toString()),
-                            tripID, expenseDate);
-                    repository.insert(expense);
-                    Toast.makeText(ExpenseDetails.this, "Expense is saved.", Toast.LENGTH_SHORT).show();
-//                    setResult(Activity.RESULT_OK);
-//                    finish();
+                // Fetch the associated trip's start and end dates
+                Trip associatedTrip = repository.getTripByID(tripID);
+                String tripStartDate = associatedTrip.getStartDate();
+                String tripEndDate = associatedTrip.getEndDate();
+
+                // Validate that the expense date is within the business trip dates
+                if (!isDateValid(expenseDate)) {
+                    Toast.makeText(ExpenseDetails.this, "Invalid expense date format. Enter mm-dd-yy", Toast.LENGTH_SHORT).show();
+                } else if (!isDateWithinRange(expenseDate, tripStartDate, tripEndDate)) {
+                    Toast.makeText(ExpenseDetails.this, "Expense date must be during the associated business trip dates", Toast.LENGTH_SHORT).show();
                 } else {
-                    expense = new Expense(id, editName.getText().toString(), Double.parseDouble(editPrice.getText().toString()),
-                            tripID, expenseDate);
-                    repository.update(expense);
-                    Toast.makeText(ExpenseDetails.this, "Expense is updated.", Toast.LENGTH_SHORT).show();
-//                    setResult(Activity.RESULT_OK);
-//                    finish();
+                    // All validation checks passed
+                    if (id == -1) {
+                        expense = new Expense(0, editName.getText().toString(), Double.parseDouble(editPrice.getText().toString()),
+                                tripID, expenseDate);
+                        repository.insert(expense);
+                        Toast.makeText(ExpenseDetails.this, "Expense is saved.", Toast.LENGTH_SHORT).show();
+                        // setResult(Activity.RESULT_OK);
+                        // finish();
+                    } else {
+                        expense = new Expense(id, editName.getText().toString(), Double.parseDouble(editPrice.getText().toString()),
+                                tripID, expenseDate);
+                        repository.update(expense);
+                        Toast.makeText(ExpenseDetails.this, "Expense is updated.", Toast.LENGTH_SHORT).show();
+                        // setResult(Activity.RESULT_OK);
+                        // finish();
+                    }
                 }
             }
         });
     } //end of onCreate
 
+    private boolean isDateWithinRange(String dateToCheck, String startDate, String endDate) {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yy", Locale.US);
+        sdf.setLenient(false);
+        try {
+            // Parse the start, end, and check dates
+            Date parsedStartDate = sdf.parse(startDate);
+            Date parsedEndDate = sdf.parse(endDate);
+            Date parsedDateToCheck = sdf.parse(dateToCheck);
+            // Check if the date is within the business trip date range
+            return (parsedDateToCheck.equals(parsedStartDate) || parsedDateToCheck.after(parsedStartDate)) &&
+                    (parsedDateToCheck.equals(parsedEndDate) || parsedDateToCheck.before(parsedEndDate));
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private boolean isDateValid(String date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yy", Locale.US);
+        sdf.setLenient(false);
+        try {
+            // Parse the date to check if it's valid
+            Date parsedDate = sdf.parse(date);
+            return true;
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_expense_details, menu);
@@ -139,6 +181,5 @@ public class ExpenseDetails extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 
 }
